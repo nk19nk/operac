7831c8757cbc15be82e7c4a7aacbd4d2cde3f1e8
nk19nk <145348064+nk19nk@users.noreply.github.com>
2023-12-25 02:35:43 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,23 +1,18 @@
 import telebot
 import random
-
 TOKEN = '6589778521:AAEq7EUmL41iCrbo38N9IQqmw5iTNMg4lAk'
-
 bot = telebot.TeleBot(TOKEN)
-
 sticks = 20  # Начальное количество палочек
-
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Добро пожаловать! Это игра в палочки. На столе лежат 20 палочек.")
-
+    bot.send_message(message.chat.id, f"Добро пожаловать! Это игра в палочки. На столе лежат 20 палочек.")
 @bot.message_handler(func=lambda message: True)
 def game(message):
     global sticks
     try:
         num = int(message.text)
         if num < 1 or num > 3:
-            bot.reply_to(message, "Можно взять только 1, 2 или 3 палочки.")
+            bot.reply_to(message, f"Можно взять только 1, 2 или 3 палочки.")
         else:
             sticks -= num
             if sticks <= 0:
@@ -34,6 +29,5 @@ def game(message):
                 else:
                     bot.send_message(message.chat.id, f"Я взял {b} палочки. Осталось {sticks} палочек.")           
     except ValueError:
-        bot.reply_to(message, "Пожалуйста, введите число от 1 до 3.")
-
+        bot.reply_to(message, f"Пожалуйста, введите число от 1 до 3.")
 bot.polling()
